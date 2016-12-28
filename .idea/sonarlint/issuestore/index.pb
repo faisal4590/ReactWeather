bf515a2f3b4624e77df088f0a0bfc0f61269d112
optimized faisal <optimized faisal>
2016-12-28 14:53:24 +0600

    App ready to publish to Heroku

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -12,3 +12,9 @@ I
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+9
+	readme.md,2\7\275d783e298228506068436512433d343feb52aa
+K
+app\components\Examples.jsx,6\8\68729802ce56cdb91ebb346c6b71d0444d3e625a
+;
+app\app.jsx,c\7\c778c85982516a0b06a364c1f502509412ab7a42
--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-var express = require('express');
+var express = require ( 'express' );
 
 // Create our app
-var app = express();
+var app = express ();
+const PORT = process.env.PORT || 5000;//heroku te to 5000 port e always cholbena..
+//tai port specify kore dilam or 5000 port e cholbe..
 
-app.use(express.static('public'));
+app.use ( function ( request, response, next ) {
+    /*.......This part is for Deploying our app to heroku......
+    * amader app ta http te chole.. https e run hobena..
+    *tai check korbo jodi http thake tahole next() e agabo..
+    * ar jodi https thake tahole take redirect kore http kore dibo...
+    * */
+    if (request.headers[ 'x-forwarded-proto' ] === 'http') {
+        next ();
+    }
+    else {
+        response.redirect('http://' + request.hostname + request.url);
+    }
+} );
 
-app.listen(5000, function () {
-  console.log('Express server is up on port 5000');
-});
+app.use ( express.static ( 'public' ) );
+
+app.listen ( PORT, function () {
+    console.log ( 'Express server is up on port ' + PORT );
+} );
