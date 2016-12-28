2aec572af77dd2af9e6affcce7c2dbb9a9f303d8
optimized faisal <optimized faisal>
2016-12-28 20:20:14 +0600

    Adding styling to Navbar by Foundation

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -5,59 +5,112 @@ var {Link, IndexLink} = require ( 'react-router' );
 //library ta nilam
 
 /*var Nav = React.createClass ( {
+ render: function () {
+ return (
+ <div>
+ <h2>Nav Component</h2>
+ <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather </IndexLink>
+ {/!*   ........this comment is for why we use IndexLink........
+ App.jsx file e gele dekhbo Main component er under e
+ baki route gulake nested kora.. sejonno IndexRoute ta always i active thake
+ onno kono route e visit korleo/... eta alada korar jonno root component orthat
+ jetake sob page ei rakhte chai sei route ke IndexLink kore dibo just..
+ ekhn ar nav e getWeather always bold hoye thakbena..
+ *!/}
+
+
+ <Link to="/About" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About </Link>
+ {/!*'/' e gele home page e jabo.. '/about'
+ e gele About page e jabo...*!/}
+ <Link to="/Examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples </Link>
+ {/!*.......This comment is for activeClassName="active" .............
+ protitay activeClassName="active" use korlam karon
+ ami suppose Example page e aci.. oi page e just Example link tai
+ active thakbe nav e.. tai sobguatei eta add korci...*!/}
+
+ {/!*........ This comment is for activeStyle={{fontWeight:'bold'}}...........
+ activeStyle={{fontWeight:'bold'}} hocce jsx expression for applying inline styling*!/}
+ </div>
+
+ );
+ }
+ } );*/
+
+var Nav = React.createClass ( {
+
+    onSearch: function ( e ) {
+        e.preventDefault ();
+        alert ( 'not wired up' );
+
+    },
+
+
     render: function () {
         return (
-            <div>
-                <h2>Nav Component</h2>
-                <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather </IndexLink>
-                {/!*   ........this comment is for why we use IndexLink........
-                 App.jsx file e gele dekhbo Main component er under e
-                 baki route gulake nested kora.. sejonno IndexRoute ta always i active thake
-                 onno kono route e visit korleo/... eta alada korar jonno root component orthat
-                 jetake sob page ei rakhte chai sei route ke IndexLink kore dibo just..
-                 ekhn ar nav e getWeather always bold hoye thakbena..
-                 *!/}
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">
+                            World Weather App
+                        </li>
 
+                        <li>
+                            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get
+                                Weather </IndexLink>
+                        </li>
+                        <li>
+                            <Link to="/About" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About </Link>
 
-                <Link to="/About" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About </Link>
-                {/!*'/' e gele home page e jabo.. '/about'
-                 e gele About page e jabo...*!/}
-                <Link to="/Examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples </Link>
-                {/!*.......This comment is for activeClassName="active" .............
-                 protitay activeClassName="active" use korlam karon
-                 ami suppose Example page e aci.. oi page e just Example link tai
-                 active thakbe nav e.. tai sobguatei eta add korci...*!/}
+                        </li>
+                        <li>
+                            <Link to="/Examples" activeClassName="active"
+                                  activeStyle={{fontWeight: 'bold'}}>Examples </Link>
 
-                {/!*........ This comment is for activeStyle={{fontWeight:'bold'}}...........
-                 activeStyle={{fontWeight:'bold'}} hocce jsx expression for applying inline styling*!/}
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="top-bar-right">
+                    <form onSubmit={this.onSearch}>
+                        <ul className="menu">
+                            <li>
+                                <input type="search" placeholder="Search Weather"/>
+                            </li>
+                            <li>
+                                <input type="submit" className="button" value="Get Weather"/>
+                            </li>
+                        </ul>
+                    </form>
+                </div>
             </div>
-
         );
     }
-} );*/
+} );
 
-var Nav = () =>{
+
+module.exports = Nav;
+
+var oldNav = () => {
     return (
         <div>
             <h2>Nav Component</h2>
             <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather </IndexLink>
-             {/* ........this comment is for why we use IndexLink........
-                App.jsx file e gele dekhbo Main component er under e
-                baki route gulake nested kora.. sejonno IndexRoute ta always i active thake
-                onno kono route e visit korleo/... eta alada korar jonno root component orthat
-                jetake sob page ei rakhte chai sei route ke IndexLink kore dibo just..
-                ekhn ar nav e getWeather always bold hoye thakbena..*/}
-
+            {/* ........this comment is for why we use IndexLink........
+             App.jsx file e gele dekhbo Main component er under e
+             baki route gulake nested kora.. sejonno IndexRoute ta always i active thake
+             onno kono route e visit korleo/... eta alada korar jonno root component orthat
+             jetake sob page ei rakhte chai sei route ke IndexLink kore dibo just..
+             ekhn ar nav e getWeather always bold hoye thakbena..*/}
 
 
             <Link to="/About" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About </Link>
             {/*'/' e gele home page e jabo.. '/about'
-                e gele About page e jabo...*/}
+             e gele About page e jabo...*/}
             <Link to="/Examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples </Link>
             {/*.......This comment is for activeClassName="active" .............
-                protitay activeClassName="active" use korlam karon
-                ami suppose Example page e aci.. oi page e just Example link tai
-                active thakbe nav e.. tai sobguatei eta add korci...*/}
+             protitay activeClassName="active" use korlam karon
+             ami suppose Example page e aci.. oi page e just Example link tai
+             active thakbe nav e.. tai sobguatei eta add korci...*/}
 
             {/* ........ This comment is for activeStyle={{fontWeight:'bold'}}...........
              activeStyle={{fontWeight:'bold'}} hocce jsx expression for applying inline styling*/}
@@ -65,5 +118,3 @@ var Nav = () =>{
 
     );
 };
-
-module.exports = Nav;
